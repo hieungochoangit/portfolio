ac8e4f7666b08040463e5d5fe1e97b2d832135f2
hieuhn <hieungochoangit@gmail.com>
2021-12-29 16:49:48 +0700

    Skill tab

---
+++ src/components/Tab/Skill/index.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import Progress from '../../Progress';
+
+const Wrapper = styled.div``
+
+function Skill(props) {
+
+  // Skills
+  const skills = [
+    {
+      id: 1,
+      name: 'HTML',
+      percent: '90%',
+    },
+    {
+      id: 2,
+      name: 'CSS',
+      percent: '70%',
+    },
+    {
+      id: 3,
+      name: 'JS',
+      percent: '60%',
+    },
+    {
+      id: 4,
+      name: 'ReactJs',
+      percent: '30%',
+    },
+    {
+      id: 5,
+      name: 'PHP - Laravel',
+      percent: '60%',
+    },
+    {
+      id: 6,
+      name: 'Database (MySql, NoSql)',
+      percent: '30%',
+    },
+    {
+      id: 7,
+      name: 'Copy - Paste',
+      percent: '100%',
+    },
+  ]
+
+  return (
+    <Wrapper>
+      {skills.map((skill) => <Progress key={skill.id} skill={skill} />)}
+    </Wrapper>
+  );
+}
+
+export default Skill;
