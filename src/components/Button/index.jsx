3ad0a934aa4e4fbcf71b2720170091714b8b5a1f
hieuhn <hieungochoangit@gmail.com>
2021-12-29 16:49:18 +0700

    Add small type button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,17 +5,17 @@ import BaseColorButton from '../BaseColorButton';
 import BaseShadowButton from '../BaseShadowButton';
 
 const ButtonBase = styled.a`
-  font-size: 22px;
+  font-size: ${props => props.small ? '16px' : '22px'};
   display: inline-block;
   margin: 4px;
   cursor: pointer;
   border-radius: 4px;
 `
 
-function Button({ text, type, href }) {
+function Button({ text, type, href, small }) {
   return (
     <BaseShadowButton>
-      <ButtonBase type={type} href={href}>
+      <ButtonBase type={type} href={href} small={small}>
         <BaseColorButton>
           {text}
         </BaseColorButton>
